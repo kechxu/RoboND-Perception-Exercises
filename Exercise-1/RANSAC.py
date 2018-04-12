36ce19a5e8943beeffc1c28f462f144591868ccd
kechxu <kechxu89@gmail.com>
2018-04-11 20:58:53 -0700

    Implement Pass Through Filter

--- Exercise-1/RANSAC.py
+++ Exercise-1/RANSAC.py
@@ -5,8 +5,7 @@ import pcl
 cloud = pcl.load_XYZRGB('tabletop.pcd')
 
 
-# Voxel Grid filter to downsample the point cloud data
-
+## Voxel Grid filter to downsample the point cloud data
 # Create a VoxelGrid filter object for our input point cloud
 vox = cloud.make_voxel_grid_filter()
 
@@ -21,8 +20,21 @@ cloud_filtered = vox.filter()
 filename = 'voxel_downsampled.pcd'
 pcl.save(cloud_filtered, filename)
 
-# PassThrough filter
+## PassThrough filter
+# Create a PassThrough filter object
+passthrough = cloud_filtered.make_passthrough_filter()
 
+# Assign axis and range to the passthrough filter object
+filter_axis = 'z'
+passthrough.set_filter_field_name(filter_axis)
+axis_min = 0.6
+axis_max = 1.1
+passthrough.set_filter_limits(axis_min, axis_max)
+
+# Use the filter function to obtain the resultant point cloud
+cloud_filtered = passthrough.filter()
+filename = 'pass_through_filtered.pcd'
+pcl.save(cloud_filtered, filename)
 
 # RANSAC plane segmentation
 

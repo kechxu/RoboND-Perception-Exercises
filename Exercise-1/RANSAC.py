db3e35038dd34c9246907c39dde10cc7585fbd15
kechxu <kechxu89@gmail.com>
2018-04-12 21:14:11 -0700

    implement outlier extraction

--- Exercise-1/RANSAC.py
+++ Exercise-1/RANSAC.py
@@ -36,18 +36,52 @@ cloud_filtered = passthrough.filter()
 filename = 'pass_through_filtered.pcd'
 pcl.save(cloud_filtered, filename)
 
-# RANSAC plane segmentation
+## RANSAC plane segmentation
+# Create the segmentation object
+seg = cloud_filtered.make_segmenter()
 
+# Set the mode lyou wish to fit
+seg.set_model_type(pcl.SACMODEL_PLANE)
+seg.set_method_type(pcl.SAC_RANSAC)
+
+# Max distance for a point to be considered fitting the model
+max_distance = 0.01
+seg.set_distance_threshold(max_distance)
+
+# Call the segment function to obtain
+# set of inlier indices and model coefficients
+inliers, coefficients = seg.segment()
 
 # Extract inliers
-
-# Save pcd for table
-# pcl.save(cloud, filename)
-
+extracted_inliers = cloud_filtered.extract(inliers, negative=False)
+filename = 'extracted_inliers.pcd'
+pcl.save(extracted_inliers, filename)
 
 # Extract outliers
+extracted_outliers = cloud_filtered.extract(inliers, negative=True)
+filename = 'extracted_outliers.pcd'
+pcl.save(extracted_outliers, filename)
 
+cloud_filtered = extracted_outliers
+
+'''
+# Outlier filter
+outlier_filter = cloud_filtered.make_statistical_outlier_filter()
+
+# Set the number of neighboring points to analyze for any given point
+outlier_filter.set_mean_k(50)
+
+# Set threshold scale factor
+x = 1.0
+
+# Any point with a mean distance larger than global
+# (mean distance + x * std_dev) will be considered outlier
+outlier_filter.set_std_dev_mul_thresh(x)
+
+# Finally call the filter function for magic
+cloud_filtered = outlier_filter.filter()
 
 # Save pcd for tabletop objects
-
-
+filename = 'cloud_filtered.pcd'
+pcl.save(cloud_filtered, filename)
+'''
